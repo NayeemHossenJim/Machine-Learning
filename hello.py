29f835db19393171a06760637e3ecb0efc22a035
NayeemHossenJim <nayeemhossenjim@gmail.com>
2024-09-06 22:25:09 +0600

    6-9-24

---
+++ hello.py
@@ -0,0 +1,32 @@
+import sys
+print(sys.version)
+
+if 5 > 2 :
+    print("Behen K laure")
+
+"""I always wonder how many times I used to learn & how many times i forgot those
+its a shame man ."""
+
+Name = ["Jim","Sojeb","Topu"]
+x,y,z = Name
+
+for s in Name:
+    print(s)
+
+print(len(s))
+
+if "Gay" in s:
+    print("Sojeb Needs Love")
+if "Gay" not in s :
+    print("Sojeb Needs Thesis")
+
+h = "Main Hu Gian"
+print(h[2:])
+
+k = "Sik Mundus Creatus Est"
+l = k.split(" ")
+print(l)
+
+o = 55
+print(f"This is my grade mark : {o}" )
+
