1aea430d70030bb389f27dfb89125a1ce4111ded
NayeemHossenJim <nayeemhossenjim@gmail.com>
2024-09-07 22:16:53 +0600

    Add Practice1

---
+++ Practice1.py
@@ -0,0 +1,17 @@
+import sys
+
+print("Hello I'm Jim")
+
+System = sys.version
+print(f"Currently I'm using Python {System}")
+
+#Whoever see this is Gay 
+
+
+Slice = "Nice & Attractive"
+print(Slice[:5])
+print(Slice.split("&"))
+
+Sojeb,Joy,Topu = ["Gay","Dukhi Atma","Sexual Psychopath"]
+if "Gay" in Sojeb:
+    print("Sojeb is Gay")
