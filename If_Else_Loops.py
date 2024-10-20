938af7ad4cb77794681bee14e545890368a0698d
NayeemHossenJim <155984091+NayeemHossenJim@users.noreply.github.com>
2024-10-21 02:32:55 +0600

    Adding Up-To Task 5

---
+++ If_Else_Loops.py
@@ -0,0 +1,12 @@
+def If_else() :
+    a = 0 
+    b = 0
+    if a > b :
+        print("A Is Bigger")
+    elif a < b :
+        print("B is Bigger")
+    else :
+        print("Error")
+
+
+MyDic = {"Sojeb" : "Gay", "Sexuality" : "Trans", "BodyCount" : "Ogonito"}
---
+++ Modules_Maths.py
@@ -0,0 +1,16 @@
+from If_Else_Loops import MyDic 
+import math
+
+Jim = MyDic["Sojeb"]
+print("Sojeb is " + Jim)
+
+sojebSbichi = math.floor(1.8)
+sojebSnunu = math.ceil(1.2)
+print("Sojeb ar Bichi: " + str(sojebSbichi))
+print("Sojeb Ar Nunu: " + str(sojebSnunu))
+
+name = input("Enter Your Name : " )
+if name == "Sojeb":
+    print("Gay")
+elif name == "Niloy":
+    print("Bisexual")
