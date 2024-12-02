78867c67e2b5f6cb8d00c24d9f803d9b30568368
NayeemHossenJim <155984091+NayeemHossenJim@users.noreply.github.com>
2024-12-02 17:06:11 +0600

    Create Gradient_Descent.py

---
+++ 5. Machine_Learning/3. Gradient_Descent/Gradient_Descent.py
@@ -0,0 +1,18 @@
+import numpy as nb
+
+def Gradient_Descent(x,y):
+    learningRate = 0.001
+    m_curr = b_curr = 0
+    iteration = 1000
+    n = len(x)
+    for i in range(iteration):
+        y_predicted = m_curr * x + b_curr
+        m_der = -(2/n)*(x*(y-y_predicted))
+        b_der = -(2/n)*(y-y_predicted)
+        m_curr = m_curr - learningRate * m_der
+        b_der = b_der - learningRate * b_der
+
+x = nb.array([1,2,3,4,5])
+y = nb.array([5,7,9,11,13])
+
+Gradient_Descent(x,y)
